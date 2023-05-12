4454cb8d8cd262ef5988d18ba5f00c49f36d4559
ines <identidad.cp@gmail.com>
2023-05-12 15:28:31 +0200

    m

---
+++ Pyton tutorial/helloworld.py
@@ -0,0 +1,15 @@
+'''
+Este es un comentario de varias lineas
+'''
+# Este es un comentario en una sola línea
+print ("Ines 29",100)
+print ("Welcome")
+
+
+'''
+cuando usamos print y queremos hacer varias lineas
+tenemos que ponerlo uno debajo de otro
+asdemas, si añadimos al string un int, veremos que 
+el espacio se genera automaticamente
+
+'''
---
+++ Pyton tutorial/variables.py
@@ -0,0 +1,9 @@
+print('Tim is a boy')
+print('Tim is 18')
+print('Tim is form turkey')
+
+name = 'Tim'
+
+print(name) #no hacen falta las comillas para poner variables 
+
+print(name+'is a boy') #si queremos poner una variable en el print, hay que poner el +
